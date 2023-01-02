9180f240e83e4c1eb5ebd3b462bf51ed984bd62c
collo <mutaic84@gmail.com>
2023-01-02 15:46:14 +0300

    :hammer: :lipstick: create custom button

---
+++ client/src/components/CustomButton.tsx
@@ -0,0 +1,21 @@
+import React, { MouseEventHandler } from 'react';
+
+interface BtnProps {
+  btnType: ('button' | 'submit' | 'reset');
+  title: string;
+  styles: string;
+  handleClick: MouseEventHandler;
+}
+
+const CustomButton = ({btnType, title, styles, handleClick}: BtnProps) => {
+  return (
+    <button
+      type={btnType}
+      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+    >
+      {title}
+    </button>
+  )
+}
+
+export default CustomButton
