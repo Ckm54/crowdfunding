62588f7f5b65422f887aa09043044e2caf1974d5
collo <mutaic84@gmail.com>
2023-01-02 16:26:29 +0300

    :hammer: :bug: close drawer

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -96,7 +96,10 @@ const Navbar = () => {
               title={address ? 'Create a campaign' : 'Connect'}
               styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
               handleClick={() => {
-                if(address) navigate('create-campaign')
+                if(address) {
+                  navigate('create-campaign');
+                  setToggleDrawer(false);
+                }
                 else connect()
               }}
             />
