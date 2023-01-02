24699f1fe4e34ad5da8e4c4b10dba3932d55d30f
collo <mutaic84@gmail.com>
2023-01-02 13:25:44 +0300

    :technologist: update file structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
 
-export default function Home() {
+import { Sidebar, Navbar } from "./components";
+import { CampaignDetails, CreateCampaign, Profile, Home } from "./pages";
+
+export default function App() {
   return (
-    <div className="container">
-      <h1>App</h1>
+    <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      {/* Sidebar wrapper */}
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+
     </div>
   );
 }
