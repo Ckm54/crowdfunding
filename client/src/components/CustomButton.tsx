54bb0d318cee30612228fabb809ffab9d9161b12
collo <mutaic84@gmail.com>
2023-01-02 16:26:07 +0300

    :bug: handle navigation

--- client/src/components/CustomButton.tsx
+++ client/src/components/CustomButton.tsx
@@ -12,6 +12,7 @@ const CustomButton = ({btnType, title, styles, handleClick}: BtnProps) => {
     <button
       type={btnType}
       className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+      onClick={handleClick}
     >
       {title}
     </button>
