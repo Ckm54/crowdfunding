01225d510818f74b262e052dc1a812be9343b36a
collo <mutaic84@gmail.com>
2023-01-03 08:30:18 +0300

    :hammer: :lipstick: component update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { DisplayCampaigns } from '../components';
+import { StateContext, ContractContextType } from '../context'
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getCampaigns } = React.useContext(StateContext) as ContractContextType;
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data: any = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if(contract) fetchCampaigns();
+  }, [address, contract]);
+
   return (
-    <div>Home</div>
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
   )
 }
 
